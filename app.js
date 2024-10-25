4f0cdc8607addff5f0cd3a344bbd8227f1f64f19
CipungAbubu <maryaheba08@gmail.com>
2024-10-25 22:10:43 +0700

    revivi script membuat file part 2

--- app.js
+++ app.js
@@ -23,12 +23,15 @@ const app = {}
 app.makeFile = () => {
     rl.question("Masukan Nama Folder: ", (folder) => {
         rl.question("Masukan Nama File: ", (file) => {
-            rl.question("Masukan extension: ", (ext) => {
-                const filePath = '${__dirname}/${folder}/$file}.${ext}';
+            rl.question("Masukan extension (misal: txt, js, etc): ", (ext) => {
+                // Menggabungkan path dengan benar
+                const filePath = `${__dirname}/${folder}/${file}.${ext}`;
 
+                // Buat folder jika belum ada
                 fs.mkdir(`${__dirname}/${folder}`, { recursive: true }, (err) => {
                     if (err) throw err;
-               
+
+                    // Buat file kosong
                     fs.writeFile(filePath, '', (err) => {
                         if (err) throw err;
                         console.log(`File berhasil dibuat: ${filePath}`);
