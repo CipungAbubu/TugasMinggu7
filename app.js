c758f449dad9600a7e23b5513da50943b3333e78
CipungAbubu <maryaheba08@gmail.com>
2024-10-25 22:07:59 +0700

    Menjalankan make-file di terminal yang ke-2

--- app.js
+++ app.js
@@ -21,12 +21,23 @@ const app = {}
 
 // Script membuat file
 app.makeFile = () => {
-    rl.question("Masukan Nama File (termasuk ekstensi): ", (fileName) => {
-        rl.question("Masukan Konten File: ", (content) => {
-            fs.writeFile(__dirname + `/${fileName}`, content, (err) => {
-                if (err) throw err;
-                console.log(`File ${fileName} berhasil dibuat!`);
-                rl.close();
+    rl.question("Masukan Nama Folder: ", (folder) => {
+        rl.question("Masukan Nama File: ", (file) => {
+            rl.question("Masukan extension (misal: txt, js, etc): ", (ext) => {
+                // Menggabungkan path dengan benar
+                const filePath = `${__dirname}/${folder}/${file}.${ext}`;
+
+                // Buat folder jika belum ada
+                fs.mkdir(`${__dirname}/${folder}`, { recursive: true }, (err) => {
+                    if (err) throw err;
+
+                    // Buat file kosong
+                    fs.writeFile(filePath, '', (err) => {
+                        if (err) throw err;
+                        console.log(`File berhasil dibuat: ${filePath}`);
+                        rl.close();
+                    });
+                });
             });
         });
     });
